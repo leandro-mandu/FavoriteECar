030c1c555f2973d5251f6c437399536c87539b75
leandro-mandu <leandro.om15@gmail.com>
2023-10-10 21:56:38 -0300

    recyclerView portado para fragment

--- app/src/main/java/com/leandromandu/favoriteecar/MainActivity.kt
+++ app/src/main/java/com/leandromandu/favoriteecar/MainActivity.kt
@@ -14,7 +14,7 @@ import com.leandromandu.favoriteecar.data.CarroFactory
 
 class MainActivity : AppCompatActivity() {
     lateinit var btn_navegar : Button
-    lateinit var car_list : RecyclerView
+    //lateinit var car_list : RecyclerView
     lateinit var tab_car : TabLayout
     lateinit var viewPager2 : ViewPager2
 
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     fun setupView(){
         btn_navegar=findViewById(R.id.bt_navegar)
-        car_list=findViewById(R.id.rv_car_list)
+        //car_list=findViewById(R.id.rv_car_list)
         tab_car=findViewById(R.id.tab_carros)
         viewPager2=findViewById(R.id.vp_view_pager)
 
@@ -36,13 +36,13 @@ class MainActivity : AppCompatActivity() {
 //        val dados = arrayOf("Car One", "Car Two", "Car Three")
  //       val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
 
-        val dados = CarroFactory.lista
-        val adapter = CarAdapter(dados)
+//        val dados = CarroFactory.lista
+//        val adapter = CarAdapter(dados)
         val tabAdapter = TabAdapter(this)
         viewPager2.adapter=tabAdapter
 //        val adapter = CustomAdapter(dados)
 //        car_list.adapter=adapter
-        car_list.adapter=adapter
+//        car_list.adapter=adapter
     }
 
     fun setupListeners(){
--- app/src/main/java/com/leandromandu/favoriteecar/fragments/CarFragment.kt
+++ app/src/main/java/com/leandromandu/favoriteecar/fragments/CarFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.leandromandu.favoriteecar.R
+import com.leandromandu.favoriteecar.adapter.CarAdapter
+import com.leandromandu.favoriteecar.data.CarroFactory
 
 class CarFragment : Fragment(){
+    lateinit var car_list : RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,5 +19,19 @@ class CarFragment : Fragment(){
     ): View? {
         return inflater.inflate(R.layout.car_fragment, container, false)
 //        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        setupView(view)
+    }
+    fun setupView(view:View){
+        val dados = CarroFactory.lista
+        val adapter = CarAdapter(dados)
+        car_list=view.findViewById(R.id.rv_car_list)
+        car_list.adapter=adapter
     }
 }
